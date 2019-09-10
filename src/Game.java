0f7b4362fe251b64e829e169502572c7caf89a05
kedtol <morakedtol@gmail.com>
2019-09-10 19:29:16 +0200

    Player interaction in progress

--- src/Game.java
+++ src/Game.java
@@ -3,35 +3,48 @@ import java.io.*;
 public class Game
 {
     private boolean quit;
-    private int round;
+    private int round = 0;
+    private int roundPlayer = 0;
     private int maxPlayers;
     private Player[] playerList;
 
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public SimpleDeck simpleDeck = new SimpleDeck();
 
     public Game(int _maxPlayers)
     {
         maxPlayers = _maxPlayers;
+        round = 0;
+        roundPlayer = 0;
         start();
     }
 
-
-    public void playRound()
+    private String consoleRead()
     {
-
-
+        try
+        {
+            return br.readLine();
+        }
+        catch (IOException e)
+        {
+            System.out.println("BufferedReader exception");
+            return "IOexception";
+        }
     }
 
     private void start()
     {
-
-
+        resetPlayerList();
+        resetSimpleDeck();
         quit = false;
         while (!quit)
         {
             System.out.println("Round started");
-            System.out.println();
+            System.out.println("Player"+roundPlayer+" starts!");
+            playerInteraction();
+            System.out.println("Player"+roundPlayer+" finished!");
         }
     }
 
@@ -49,5 +62,31 @@ public class Game
 
     }
 
+    private void resetSimpleDeck()
+    {
+
+    }
+
+    private void loadDefaultDeck()
+    {
+
+
+    }
+
+    private void playerInteraction()
+    {
+        String command;
+        System.out.println("Type 'end' to end your round.");
+        //playerList[roundPlayer].draw
+        command = consoleRead();
+
+        while (command.equals("end"))
+        {
+
+
+            command = consoleRead();
+        }
+
+    }
 }
 
