98a136fc25d44ad01f961c46af0275dcaeaf037c
kedtol <morakedtol@gmail.com>
2019-10-10 08:16:25 +0200

    SimpleDeck now fills and shuffles.

--- src/SimpleDeck.java
+++ src/SimpleDeck.java
@@ -1,11 +1,23 @@
+import java.util.Collections;
+
 public class SimpleDeck extends GameObject
 {
-    public static Deck dumpDeck;
-    public static Deck deck;
+    public Deck dumpDeck = new Deck();
 
-    public void generate()
+
+    public void generate(DefaultDeck defaultDeck)
     {
 
 
+        for (int i = 0; i < defaultDeck.defaultCards.size(); i++)
+        {
+            deck.addDeckCapacity(defaultDeck.defaultCards.get(i).amount);
+            for (int i1 = 0; i1 < defaultDeck.defaultCards.get(i).amount; i1++)
+            {
+                deck.add(defaultDeck.defaultCards.get(i));
+            }
+        }
+
+        deck.shuffle();
     }
 }
